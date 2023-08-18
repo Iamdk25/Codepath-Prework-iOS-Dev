462159995476eba2b28f118242302de3252f5c99
Iamdk25 <dkarmariya@usf.edu>
2023-08-18 03:28:27 -0400

    Commited files

--- Codepath Prework iOS Dev/ViewController.swift
+++ Codepath Prework iOS Dev/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        label1.textColor = randomColor
+        label2.textColor = randomColor
+        label3.textColor = randomColor
+        
+    }
+    
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
 }
+
+
+
+
+
 
